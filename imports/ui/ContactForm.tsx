99577fba3075422c7061b4f09f8ac867381a444c
Vitor <vi.souza.almeida@protonmail.com>
2023-06-14 16:44:38 -0300

    style(tailwind): install tailwind and header component

--- imports/ui/ContactForm.tsx
+++ imports/ui/ContactForm.tsx
@@ -19,33 +19,55 @@ export const ContactForm = () => {
     }
 
     return (
-        <form>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input
-                    id='name'
-                    value={name}
-                    onChange={(e) => { setName(e.target.value) }}
-                    type="text" />
+        <form className="mt-6">
+            <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                    <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                        Name
+                    </label>
+                    <input
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                </div>
+
+                <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                        Email
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                </div>
+
+                <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                    <label htmlFor="imageUrl" className="block text-sm font-medium text-gray-700">
+                        Image URL
+                    </label>
+                    <input
+                        type="text"
+                        id="imageUrl"
+                        value={imageUrl}
+                        onChange={(e) => setImageUrl(e.target.value)}
+                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                </div>
             </div>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input
-                    id='email'
-                    value={email}
-                    onChange={(e) => { setEmail(e.target.value) }}
-                    type="email" />
-            </div>
-            <div>
-                <label htmlFor="imageUrl">image URL</label>
-                <input
-                    id='imageUrl'
-                    value={imageUrl}
-                    onChange={(e) => { setImageUrl(e.target.value) }}
-                    type="text" />
-            </div>
-            <div>
-                <button type='button' onClick={saveContact}>Save Contact</button>
+            <div className="px-2 py-3 text-right">
+                <button
+                    type="button"
+                    onClick={saveContact}
+                    className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+                >
+                    Save Contact
+                </button>
             </div>
         </form>
     )
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+    "./imports/ui/**/*.{js,jsx,ts,tsx}",
+    "./client/*.html",
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
+
