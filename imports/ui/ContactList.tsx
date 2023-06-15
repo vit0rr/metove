684a7ec33ae80e18cbf6aa28d39cc43b03e417cc
Vitor <vi.souza.almeida@protonmail.com>
2023-06-15 18:21:38 -0300

    feat(list): use memo and loading const

--- imports/ui/ContactList.tsx
+++ imports/ui/ContactList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
@@ -10,13 +10,14 @@ export const ContactList = () => {
     const noDataAvailable = { contacts: [] };
 
     const handler = Meteor.subscribe('contacts');
+    const isLoading = !handler.ready();
     if (!handler.ready()) {
-      return { ...noDataAvailable, isLoading: true };
+      return { ...noDataAvailable, isLoading };
     }
 
     const contacts = ContactsCollection.find({}, { sort: { createdAt: -1 } }).fetch();
 
-    return { contacts };
+    return { contacts, isLoading };
   });
 
   const removeContact = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, _id: string) => {
@@ -24,33 +25,50 @@ export const ContactList = () => {
     Meteor.call('contacts.remove', _id);
   };
 
+  const ContactItem = memo(
+    ({ contact }: { contact: { name: string; email: string; imageUrl: string; _id: string } }) => {
+      return (
+        <li className="py-4 flex items-center justify-between space-x-3">
+          <div className="min-w-0 flex-1 flex items-center space-x-3">
+            <div className="flex-shrink-0">
+              <img className="h-10 w-10 rounded-full" src={contact.imageUrl} alt="" />
+            </div>
+            <div className="min-w-0 flex-1">
+              <p className="text-sm font-medium text-gray-900 truncate">{contact.name}</p>
+              <p className="text-sm font-medium text-gray-500 truncate">{contact.email}</p>
+            </div>
+            <div>
+              <a
+                href=""
+                onClick={(e) => removeContact(e, contact._id)}
+                className="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
+              >
+                Remove
+              </a>
+            </div>
+          </div>
+        </li>
+      );
+    },
+  );
+
   return (
     <div>
       <div className="mt-10">
         <h3 className="text-xs font-semibold text-gray-500 uppercase tracking-wide">Contact List</h3>
         <ul className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200">
-          {contacts.map((person: { name: string; email: string; imageUrl: string; _id: string }, personIdx: string) => (
-            <li key={personIdx} className="py-4 flex items-center justify-between space-x-3">
-              <div className="min-w-0 flex-1 flex items-center space-x-3">
-                <div className="flex-shrink-0">
-                  <img className="h-10 w-10 rounded-full" src={person.imageUrl} alt="" />
-                </div>
-                <div className="min-w-0 flex-1">
-                  <p className="text-sm font-medium text-gray-900 truncate">{person.name}</p>
-                  <p className="text-sm font-medium text-gray-500 truncate">{person.email}</p>
-                </div>
-                <div>
-                  <a
-                    href=""
-                    onClick={(e) => removeContact(e, person._id)}
-                    className="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
-                  >
-                    Remove
-                  </a>
-                </div>
+          {isLoading && (
+            <div>
+              <div className="mt-10">
+                <h3 className="text-xs font-semibold text-gray-500 uppercase tracking-wide ">Loading...</h3>
               </div>
-            </li>
-          ))}
+            </div>
+          )}
+          {contacts.map(
+            (contact: { name: string; email: string; imageUrl: string; _id: string }, contactIdx: string) => (
+              <ContactItem key={contactIdx} contact={contact} />
+            ),
+          )}
         </ul>
       </div>
     </div>
